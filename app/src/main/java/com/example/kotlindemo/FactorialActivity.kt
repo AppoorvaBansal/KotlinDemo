9f921674200497db50497d1c5d2d14e664bfc325
Appoorva Bansal <appoorva.ag104@gmail.com>
2021-08-13 16:15:41 +0530

    update

---
+++ app/src/main/java/com/example/kotlindemo/FactorialActivity.kt
@@ -0,0 +1,38 @@
+package com.example.kotlindemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_factorial.*
+
+class FactorialActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_factorial)
+
+       val ednum=findViewById<EditText>(R.id.ednum);
+        var btnfact=findViewById<Button>(R.id.btnfact);
+        val txtres=findViewById<TextView>(R.id.txtresult)
+
+    }
+
+    fun fact(view:View)
+    {
+       val num:Int=(ednum.text.toString()).toInt()
+        var facto:Long=1
+        for(i in 1..num)
+        {
+            facto=facto*i.toLong()
+        }
+
+        txtresult.text=facto.toString()
+
+
+
+    }
+}
